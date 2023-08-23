354e1b030d5b8581b97ba9f4c92b1baa23d5156a
softdevelopp <softdevelopp@gmail.com>
2023-08-23 16:25:56 -0500

    capacitacion Eduardo Ortiz

---
+++ data/data.js
@@ -0,0 +1,66 @@
+export const data = [
+  {
+    question: "Ley 1801 de 2016?",
+    choices: ["Código Nacional de Seguridad y Convivencia Ciudadana", "Trata de Titulo Minero","Fuerza bruta a la ciudadania", "Habla de Despido Policial"],
+    answer: "Código Nacional de Seguridad y Convivencia Ciudadana",
+  },
+  {
+    question: "¿Cuáles son los principios de la cultura ciudadana?",
+    choices: [" Corresponsabilidad ", "Auto y mutua regulacion", "cambio cultural", "Respetar la diversidad"],  
+    answer: "Auto y mutua regulacion",
+  },
+  {
+    question: "¿En qué consiste el lineamiento de Corresponsabilidad ciudadana?",
+    choices: ["Es importante que asumamos como costumbre resolver las diferencias de manera pacífica a través del diálogo.", "Anime a las personas a comunicarse y comprender las perspectivas de los demás.", "Es importante que cada persona asuma su responsabilidad y actúe de manera responsable y respetuosa en el espacio público."],
+    answer: "Es importante que cada persona asuma su responsabilidad y actúe de manera responsable y respetuosa en el espacio público.",
+  },
+
+  {
+    question: "3.	¿De los siguientes cuáles son desafíos de la cultura ciudadana?",
+    choices: ["Cambio cultural, Cambio de comportamiento", "Falta de comunicación, Falta de compromiso", "Falta de participación ciudadana, Resistencia al cambio"],
+    answer: "Falta de participación ciudadana, Resistencia al cambio",
+  },
+
+  {
+    question: "¿Cual es una de las acciones para evitar las riñas?",
+    choices: ["Respetar la diversidad", "Comparendos", "Tener Honestidad", "Lealtad"],
+    answer: "Respetar la diversidad",
+  },
+
+  {
+    question: "¿Cual es un desafio de la cultura ciudadana?",
+    choices: ["Respetar la autoridad", "Corresponsabilidad", "La aplicacion de la ley", "Falta de participacion ciudadana"],
+    answer: "Falta de participacion ciudadana ",
+  },
+
+  {
+    question: "¿En que articulo se encuentra los comportamientos contrarios a la convivencia?",
+    choices: ["Art 50", "Art 34", "Art 27", "Art 28"],
+    answer: "Art 27",
+  },
+
+  {
+    question: "La cultura ciudadana se construye sobre el aprendizaje constante, siendo importante aprender algo nuevo de cultura ciudadana cada día. ¿Segun el texto a que hace referencia?",
+    choices: ["Aprendizaje constante", "Honestidad", "Constante cambio", "Diversidad de conocimientos"],
+    answer: "Aprendizaje constante",
+  },
+
+  {
+    question: "¿Cuantos lineamientos existen para mejorar la cultura ciudadana?",
+    choices: ["8", "5", "3", "2"],
+    answer: "3",
+  },
+
+  {
+    question: "¿A que hace referencia CAMBIO CULTURAL?",
+    choices: ["La promoción de la cultura ciudadana implica un cambio cultural, es necesario, que cada persona contribuya desde su rol como ciudadano en este cambio de comportamientos en el espacio público. ", "En este sentido es necesario comprender e o interiorizar la necesidad de aportar al cambio.", "solo con el compromiso de todos podemos cambiar el pensamiento cultural en nuestra ciudad."],
+    answer: "La promoción de la cultura ciudadana implica un cambio cultural, es necesario, que cada persona contribuya desde su rol como ciudadano en este cambio de comportamientos en el espacio público.",
+  },
+
+  {
+    question: "¿Que es la falta de compromiso?",
+    choices: ["algunos ciudadanos y funcionarios públicos es otro de los desafíos que enfrenta la promoción de la cultura ciudadana.", "es un proceso que requiere la participación activa de todos los ciudadanos y funcionarios públicos ", "es un conjunto de valores, actitudes, comportamientos costumbres, acciones y reglas mínimas compartidas que generan sentido de pertenencia"],
+    answer: "algunos ciudadanos y funcionarios públicos es otro de los desafíos que enfrenta la promoción de la cultura ciudadana.",
+  },
+
+];
